1bb0bc243d7d150ac13f61fa48cba2cbb81593f9
certilremy <certilremy@gmail.com>
2020-03-23 14:11:43 -0400

    user can now create time to track

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,21 @@
+class TransactionsController < ApplicationController
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(param_transaction)
+    #@transaction.user_id = User.last
+    if @transaction.save
+      redirect_to @transaction
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def param_transaction
+    params.require(:transaction).permit(:name, :amoun, :user_id)
+  end
+end
