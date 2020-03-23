caab5968538402921bf43a873b127836f3cca0c1
certilremy <certilremy@gmail.com>
2020-03-23 13:05:52 -0400

    user can now signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(param_user)
+    if @user.save
+
+    redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  def param_user
+  params.require(:user).permit(:username)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'pages#home'
 get "/signup", to: "users#new"
-post "/signup", to: "users#create"
+resources :users, only: %i[create show]
 end
