c9e8e2e6157dad52072c869fd09a5632cec29913
certilremy <certilremy@gmail.com>
2020-03-23 15:33:30 -0400

    add validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 4, maximum: 30 }
+  validates :amoun, presence: true
   belongs_to :user
   belongs_to :group, optional: true
 end
