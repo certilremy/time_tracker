049060901a91d36159b20000a4aa4ee1673b4f48
certilremy <certilremy@gmail.com>
2020-03-23 20:05:38 -0400

    add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validate username' do
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username).is_at_most(30) }
+  end
+
+  describe 'Test associations' do
+    it { should have_many(:transactions) }
+    it { should have_many(:groups) }
+  end
 end
