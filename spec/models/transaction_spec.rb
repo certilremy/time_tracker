4d499c987025591158aa4e236f2a2adbd0b95e59
certilremy <certilremy@gmail.com>
2020-03-23 20:10:31 -0400

    add test for transaction

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validation' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(30) }
+    it { should validate_presence_of(:amoun) }
+  end
+  describe 'associations to test' do
+    it { should belong_to(:user) }
+    it { should belong_to(:group) }
+  end
 end
