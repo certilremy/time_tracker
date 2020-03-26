f9a0780f85d54f385f1f2008ae0e4f61280cb620
certilremy <certilremy@gmail.com>
2020-03-26 11:29:45 -0400

    add login style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(param_user)
     if @user.save
-
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render 'new'
