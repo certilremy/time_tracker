2fdb2ef5108a30cc924a9182b4373efcbafa0df1
certilremy <certilremy@gmail.com>
2020-03-28 01:56:03 -0400

    add validation for negative number in transaction amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,14 @@
 class Transaction < ApplicationRecord
   validates :name, presence: true, length: { minimum: 4, maximum: 30 }
   validates :amoun, presence: true
+  validate :correct_amount
   belongs_to :user
-  belongs_to :group, optional: true
+  belongs_to :group, optional: true 
+
+  private 
+
+  def correct_amount
+    errors.add(:amoun, 'must be greater than 0') if amoun && amoun <= 0
+  end
+
 end
