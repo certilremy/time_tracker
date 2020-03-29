1d9dabbc72622d025eecd71a8aecfaa4f63fbe53
certilremy <certilremy@gmail.com>
2020-03-28 23:37:48 -0400

    add controller test for user

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  let(:user) do
+    User.create(
+      username: 'john'
+    )
+  end
+
+  it 'assigns a new user to @user' do
+    get :new
+    expect(assigns(:user)).to be_a_new(User)
+  end
+
+  it 'action SHOW should redirect to root page if not logged in' do
+    get :show, params: { id: user.id }
+    expect(response).to redirect_to(root_path)
+  end
+end
